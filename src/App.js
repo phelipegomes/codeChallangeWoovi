2c3b3601da6a882d1bd70671922165ca5c042d25
Phelipe Franca <phelipecedaspy@gmail.com>
2024-07-24 17:10:35 -0300

    modificando main

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const FirstBox = () => {
       <div className="margin-spacer"><span className="pix-tag">Pix</span></div>
         <div className="price-box">
         <p><em>1x</em> R$ 30.500,00</p>
+        <p style={{color: '#3adba9', fontSize: 10
+        }}>Ganhe 3% de Cashback</p>
         <p className="cash-back-tag"></p>
         <div className="flag">
           @ <em>R$ 300,00</em> de volta no seu Pix na hora
@@ -31,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <div className='main'>
-        <h1 style={{textAlign: "center"}}>Hello [name] !</h1>
+        <h1 style={{textAlign: "center"}}>João, como você quer pagar?</h1>
         <label htmlFor="first-box-radio" className="radio-label">
           <div className="radio-container">
             <FirstBox />
