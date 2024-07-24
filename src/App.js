90c0be1d0dda191b15d41eb591405ed4e452c8ca
Phelipe Franca <phelipecedaspy@gmail.com>
2024-07-24 16:47:06 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+
+const FirstBox = () => {
+  return (
+    <div className="avista-box">
+      <div className="margin-spacer"><span className="pix-tag">Pix</span></div>
+        <div className="price-box">
+        <p><em>1x</em> R$ 30.500,00</p>
+        <p className="cash-back-tag"></p>
+        <div className="flag">
+          @ <em>R$ 300,00</em> de volta no seu Pix na hora
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const OtherBox = () => {
+  return (
+    <div className="parcelado-box">
+      <div className="margin-spacer"><span className="pix-tag">Pix Parcelado</span></div>
+      <div className="price-box">
+        <p><em>1x</em> R$ 30.500,00</p>
+        <p className="cash-back-tag"></p>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main'>
+        <h1 style={{textAlign: "center"}}>Hello [name] !</h1>
+        <label htmlFor="first-box-radio" className="radio-label">
+          <div className="radio-container">
+            <FirstBox />
+            <input type="radio" id="first-box-radio" name="payment-option" value="avista" />
+          </div>
+          
+        </label>
+        <label htmlFor="other-box-radio" className="radio-label">
+          <div className="radio-container">
+            <OtherBox />
+            <input type="radio" id="other-box-radio" name="payment-option" value="parcelado" />
+          </div>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default App;
