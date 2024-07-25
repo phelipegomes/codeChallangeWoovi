fda1f6f6bc717b1befa81984cac095e0b53fb993
Phelipe Franca <phelipecedaspy@gmail.com>
2024-07-25 13:39:52 -0300

    componentizando

---
+++ src/components/parcelado/ParceladoBox.js
@@ -0,0 +1,13 @@
+import "./parcelado.css"
+
+export default function OtherBox() {
+    return (
+        <div className="parcelado-box">
+        <div className="margin-spacer"><span className="pix-tag">Pix Parcelado</span></div>
+        <div className="price-box">
+            <p><em>1x</em> R$ 30.500,00</p>
+            <p className="cash-back-tag"></p>
+        </div>
+        </div>
+    );
+};
