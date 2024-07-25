7a9998bc1d55320b954e924c1e1b625a86351ddc
Phelipe Franca <phelipecedaspy@gmail.com>
2024-07-25 15:09:13 -0300

    adicionando logo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className='main'>
+      <img src="https://woovi.com/_next/static/media/woovi-logo.1612aac8.webp" style={{width: 150, display: "block", margin: "auto"}}/>
         <h1 style={{textAlign: "center"}}>João, como você quer pagar?</h1>
             <FirstBox />
             <OtherBox />
